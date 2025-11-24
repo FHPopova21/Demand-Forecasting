89530a7549d62c0bb1a6432756c787e20ea0f415
FHpopova21 <FHPopova21@codingburgas.bg>
2025-11-24 11:13:32 +0200

    Add M5 Forecast Documentation

--- src/_setup.py
+++
@@ -1,6 +0,0 @@
-﻿# src/_setup.py  (по желание: един ред в началото на всеки notebook/script)
-import os, pandas as pd
-from src.config import set_all_seeds, DISPLAY_MAX_ROWS, PANDAS_FLOAT_FMT
-set_all_seeds()
-pd.options.display.max_rows = DISPLAY_MAX_ROWS
-pd.options.display.float_format = PANDAS_FLOAT_FMT
